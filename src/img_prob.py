50aad8473a2044ee26caeba246b98d010eb43b1e
jsemric <jakubsemric@gmail.com>
2018-04-27 15:54:17 +0200

    structure

---
+++ src/img_prob.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from glob import glob
+import re
+import sys, os
+import numpy as np
+from scipy import misc
+import pickle
+
+def score(model, path):
+    fnames = sorted(glob(path))
+    for fname in fnames:
+        img = np.array(misc.imread(fname)).flatten()
+        preds = model.predict_proba([img])
+        preds = preds[0]
+        print(re.sub('\..*','', os.path.basename(fname)), preds[1],
+            int(preds[0] < preds[1]))
+def main():
+    fname = 'model_experiment'
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+    with open(fname,'rb') as f:
+        model = pickle.load(f)
+    # print: filename soft-decision hard-decision
+    score(model, '../eval/*.png')
+
+main()
--- src/train_model_images.py
+++ src/train_model_images.py
@@ -18,10 +18,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
 
-negative_train_path = 'data/non_target_train/*.png'
-positive_train_path = 'data/target_train/*.png'
-negative_test_path = 'data/non_target_dev/*.png'
-positive_test_path = 'data/target_dev/*.png'
+negative_train_path = '../data/non_target_train/*.png'
+positive_train_path = '../data/target_train/*.png'
+negative_test_path = '../data/non_target_dev/*.png'
+positive_test_path = '../data/target_dev/*.png'
 
 def read_img(path):
     return np.array([np.array(misc.imread(f)) for f in glob(path)])
@@ -49,7 +49,6 @@ def get_data():
     return x_train, y_train, x_test, y_test
 
 def cross_val(model,display=False):
-    #'''
     X1, y1, X2, y2 = get_data()
     X = np.concatenate((X1, X2), axis=0)
     y = np.concatenate((y1, y2), axis=0)
@@ -72,9 +71,7 @@ def cross_val(model,display=False):
     res = cross_val_score(model, X, y, cv=KFold(n_splits=5,random_state=7))
     print('Folds: ', res)
     print('mean: ', res.mean(), 'std: ', res.std())
-    #'''
     # display the result of cross-validation
-    #res = np.array([.957 , .946 , 0.967 , 0.963 , 0.9713])
     res *= 100
     if display:
         base = min((85,min(res)-1))
@@ -86,7 +83,7 @@ def cross_val(model,display=False):
         ax.set_ylabel('accuracy')
         ax.set_yticks(np.arange(base,100,1))
         plt.show()
-
+        
     # split to avoid over-fitting or perhaps train on the whole dataset
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
         random_state=78)
@@ -100,9 +97,13 @@ def cross_val(model,display=False):
 
 def main():
     model = RandomForestClassifier()
+    # any argument changes the model type to SGD
     if len(sys.argv) > 1:
         print('Using SGD classifier.')
-        model = SGDClassifier(loss='log',penalty='l2')
+        #loss = 'log'
+        loss = 'modified_huber'
+        model = SGDClassifier(loss=loss, penalty='l2')
+
     cross_val(model,display=True)
 
 if __name__ == "__main__":
